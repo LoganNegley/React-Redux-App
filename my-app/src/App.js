909bbae4028e9ca504c3037a27aeedbbf0d83ba9
Logan Negley <logannegley@gmail.com>
2020-03-03 20:28:24 -0600

    Pokemon comp rednered in app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {connect } from 'react-redux';
 import {pokemonReducer} from '../src/reducers/index';
 import './App.css';
+import Pokemon from '../src/components/Pokemon';
 
 function App(props) {
   return (
     <div className="App">
       <h1>Get Info Of A Random Pokemon</h1>
+      <Pokemon/>
     </div>
   );
 }
