9ea9dca359cae060cfbb35bd009e6165d6e7699c
Logan Negley <logannegley@gmail.com>
2020-03-03 20:58:05 -0600

    fetching pokemon data with axios

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,6 +1,10 @@
+import axios from 'axios';
+
 export const fetchPokemon = () => {
     return dispatch => {
         dispatch({type: 'FETCHING_POKEMON_START'})
-        
+        axios.get('https://pokeapi.co/api/v2/pokemon/')
+        .then(response => console.log(response))
+        .catch(error => console.log(error, 'error at pokemon api call'))
     }
 }
--- my-app/src/components/Pokemon.js
+++ my-app/src/components/Pokemon.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {connect } from 'react-redux';
 import {fetchPokemon} from '../actions/index';
+import Loader from 'react-loader-spinner';
 
 const Pokemon = (props)=>{
     return (
         <div>
         <button onClick={props.fetchPokemon}>Lets Catch A New Pokemon</button>
-           {!props.pokemon && !props.isLoading && <h2>Catch a new Pokemon</h2>}
+           {!props.pokemon && !props.isLoading && (
+               <h2>Catch a new Pokemon</h2>
+           )}
+        {props.isLoading && (
+            <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000}
+            />
+        )}
         </div>
     );
 };
