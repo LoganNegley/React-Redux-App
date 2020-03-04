56397970b45ef35764336c41c3efaf935da61fb3
Logan Negley <logannegley@gmail.com>
2020-03-03 20:26:39 -0600

    Pokemon compnonent created

---
+++ my-app/src/components/Pokemon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pokemon = ()=>{
+    return (
+        <div>
+            <h2> Coming from Pokemon Component</h2>
+        </div>
+    );
+};
+
+export default Pokemon;
